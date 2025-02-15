8766709eae8c69776e71a0fecaf98ea75985fb50
SahanUpendraAcedemic <dimithripez@gmail.com>
2025-02-15 21:37:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <main className="p-5 min-h-screen"></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
