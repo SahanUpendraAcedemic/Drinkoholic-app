d2352154142086638bfec9e064b8e4e4958e2238
SahanUpendraAcedemic <IT22600370@my.sliit.lk>
2025-02-24 15:30:27 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,18 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: "https://drinkoholic.netlify.app/",
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "src\main.jsx",
+    },
+  },
 });
